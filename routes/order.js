0aa3c18bd525a05968f81c76219fdfc947fbf69f
quan-nguyen-dinh <86878570+quan-nguyen-dinh@users.noreply.github.com>
2024-10-06 19:40:00 +0700

    add  update order api

--- routes/order.js
+++ routes/order.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 router.get('/show', OrderController.show);
 router.get('/recentOrder/:userId', OrderController.getByUserId);
 router.post('/create', OrderController.create);
+router.put('/update/:id', OrderController.update);
 router.delete('/delete', OrderController.delete);
 
 module.exports = router;
