13dbecfdf9849df7ca40c9b83586a8de872f6ecc
quan nguyen dinh <quantalaquan@gmail.com>
2024-10-26 10:13:40 +0700

    format index

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./config/db");
 const route = require("./routes");
-require('dotenv').config();
 const swaggerDocs = require('./swagger');
+
+require('dotenv').config();
+
 BigInt.prototype.toJSON = function() {
     return this.toString()
 } 
@@ -17,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 route(app);
+
 const http = require("http").Server(app);
 const port = 3001;
 
